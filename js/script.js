4e45ee937fff8e460a7492df3a8ef4f6629ccced
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-21 14:42:11 +0100

    [UPD] Add split,  reverse and join

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 //permetto all'utente di inserire una parola
 //creo una funzione per verificare che sia palindroma
 
-//faccio inserire una parola
-const parola = prompt("Inserisci una parola");
+//inserisco una funzione
+function verificaPalindromo() {
 
+    //faccio inserire una parola
+    const parola = prompt("Inserisci una parola");
+    
+    if(!parola){
+        alert("Attenzione! Inserisci una parola")
+        return;
+    }
+    
+    const palindromo= parola.split('').reverse().join('');
+
+}
+verificaPalindromo();
