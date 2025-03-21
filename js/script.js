ea166540d580ef8ed5de11edf86604d18af80ae5
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-21 14:50:47 +0100

    [UPD] Complete task 1

--- js/script.js
+++ js/script.js
@@ -6,13 +6,20 @@ function verificaPalindromo() {
 
     //faccio inserire una parola
     const parola = prompt("Inserisci una parola");
-    
+    //verifico che l'utente inserisca la parola
     if(!parola){
         alert("Attenzione! Inserisci una parola")
         return;
     }
-    
+    //creo una costante per invertire le parole
     const palindromo= parola.split('').reverse().join('');
+    //controllo quali sono originali e quali invertite
+    if(parola.toLocaleLowerCase()=== palindromo.toLocaleLowerCase()){
+        alert(`"${parola} " è una parola palindroma!`)
+    }
+    else{
+        alert(`"${parola} " non è una parola palindroma!`)
+    }
 
 }
 verificaPalindromo();
